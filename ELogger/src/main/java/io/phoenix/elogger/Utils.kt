43bda1e9f078457514903b59fd62be517ebea76d
Ehsan <ehsansouri23@gmail.com>
2020-09-12 13:59:17 +0430

    add get newest log file

--- ELogger/src/main/java/io/phoenix/elogger/Utils.kt
+++ ELogger/src/main/java/io/phoenix/elogger/Utils.kt
@@ -1,14 +1,21 @@
 package io.phoenix.elogger
 
 import android.content.Context
+import android.net.Uri
 import android.os.Environment
+import androidx.core.net.toUri
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
 fun getFile(context: Context, fileName: String): File =
     File(
-        context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+        File(
+            context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),
+            LOGS_DIR
+        ).also {
+            it.mkdirs()
+        },
         fileName
     ).let { file ->
         if (!file.exists())
@@ -16,4 +23,19 @@ fun getFile(context: Context, fileName: String): File =
         file
     }
 
-val dateFormatter = SimpleDateFormat("MMMM d,  HH-mm-ss", Locale.ENGLISH)
+val dateFormatter = SimpleDateFormat("y-m-d,  HH:mm:ss", Locale.ENGLISH)
+const val LOGS_DIR = "logs"
+
+fun getNewestFile(context: Context, fileName: String): Uri? =
+    context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        ?.let {
+            File(it, LOGS_DIR)
+                .walk()
+                .sortedByDescending {
+                    it.name
+                        .substringAfterLast("_")
+                        .substringBeforeLast(".txt")
+                }.firstOrNull {
+                    it.name.contains(fileName)
+                }?.toUri()
+        }
